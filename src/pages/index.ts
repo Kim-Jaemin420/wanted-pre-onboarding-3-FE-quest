0a9eeee0dee01e1b6ea810f350878fcd85371170
Kim-Jaemin420 <jany240@naver.com>
2023-03-02 22:23:47 +0900

    Chore: 생성된 페이지들 export

---
+++ src/pages/index.ts
@@ -0,0 +1,4 @@
+export { default as PageA } from './PageA/PageA';
+export { default as PageB } from './PageB/PageB';
+export { default as PageC } from './PageC/PageC';
+export { default as Login } from './Login/Login';
