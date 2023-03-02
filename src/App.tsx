9f9734ee0aab38d1172a04a53648fca9804a4a56
Kim-Jaemin420 <jany240@naver.com>
2023-03-02 22:23:47 +0900

    Feat: 생성한 페이지 라우트 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,20 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { Login, PageA, PageB, PageC } from './pages';
+import { MainLayout } from '@/components';
+
 function App() {
-  return <div>react</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route element={<MainLayout />}>
+          <Route path="/pageA" element={<PageA />} />
+          <Route path="/pageB" element={<PageB />} />
+          <Route path="/pageC" element={<PageC />} />
+        </Route>
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
