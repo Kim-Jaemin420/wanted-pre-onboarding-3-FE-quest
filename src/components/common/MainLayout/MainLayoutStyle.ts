620c08a8c3a7f7c28981a3de06a6a77618b241e1
Kim-Jaemin420 <jany240@naver.com>
2023-03-02 22:23:47 +0900

    Style: main layout에서 sidebar 추가시 필요한 스타일 추가

---
+++ src/components/common/MainLayout/MainLayoutStyle.ts
@@ -0,0 +1,12 @@
+import { styled } from '@stitches/react';
+
+export const Container = styled('div', {
+  height: '100vh',
+  overflow: 'hidden',
+});
+
+export const MainContainer = styled('div', {
+  display: 'flex',
+  flexFlow: 'row nowrap',
+  height: '100%',
+});
